29c0c3e472ca24dcc741c39259f74ce9d07dfb0b
Qambar-12 <144834362+Qambar-12@users.noreply.github.com>
2023-12-02 20:53:47 +0500

    Update Valid Anagram.py
    Added comments for explaination

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,5 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #Making list to let use sort method
+        #lower() method used for strings to create a lower case string 
+        #upper() method can also be used instead
+        #Nested calls
         s = list(s.lower())
         t = list(t.lower())       
         s.sort()
