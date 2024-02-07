3913cf2f4d59a87012a538fae3424e5a53c02aaf
Muhammad Qambar Hussain <144834362+Qambar-12@users.noreply.github.com>
2024-02-07 19:34:36 +0500

    Add files via upload

---
+++ 283.Move Zeroes (Two Pointer).py
@@ -0,0 +1,10 @@
+def moveZeroes(nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r] = nums[r],nums[l]
+                l += 1
+        return nums 
