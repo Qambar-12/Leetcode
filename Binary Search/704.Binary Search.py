7f8548851af809e4c7bb92dd345c7d989e750084
Muhammad Qambar Hussain <144834362+Qambar-12@users.noreply.github.com>
2024-02-17 17:05:04 +0500

    Create 704.Binary Search.py

---
+++ Binary Search/704.Binary Search.py
@@ -0,0 +1,11 @@
+def binSearch(nums,target):
+    i,j = 0,len(nums)-1
+    while i <= j:
+        index = int((i+j)/2)
+        if nums[index] == target:
+            return index
+        elif nums[index] > target:
+            j = index-1
+        else:
+            i =  index+1
+    return -1            
