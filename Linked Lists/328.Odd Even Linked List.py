623d5c4b2e24089929261da5b0de348a6ae4261a
Qambar-12 <144834362+Qambar-12@users.noreply.github.com>
2024-04-23 14:40:32 +0500

    Simply connect next of odd to head of even part at the end

---
+++ Linked Lists/328.Odd Even Linked List.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Base case
+        if not head or not head.next :
+            return head
+        odd = head
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
+        odd.next = evenHead
+        return head     
+        
