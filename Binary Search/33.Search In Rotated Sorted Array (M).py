6fa2ffe7e0f562bfa6223bcda1d117dd1d4fbc85
Muhammad Qambar Hussain <144834362+Qambar-12@users.noreply.github.com>
2024-08-21 00:15:54 +0500

    two portions of sorted arrays to check for

---
+++ Binary Search/33.Search In Rotated Sorted Array (M).py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #there are possibly two sorted portions in the array and runtime of O(logn) usually refers to binary search
+        #the portions are namely left and right and we check for these portions by:
+        #nums[l] <= mums[mid] then our pointer is in left sorted portion and right otherwise
+        #if sorted array is rotated n times it preserves the order of elements
+        if nums[0] <= nums[len(nums)-1]:
+            return nums.index(target) if target in nums else -1
+        #Binary search algorithm
+        l , r = 0 , len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if target == nums[mid]:
+                return mid
+            #if in left sorted portion
+            if nums[l] <= nums[mid]:
+                #need to search to the right 
+                if target > nums[mid] or target < nums[l] :
+                    l = mid + 1
+                else:
+                    r = mid - 1    
+            #if in right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1    
+        return -1    
